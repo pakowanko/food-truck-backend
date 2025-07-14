ef6b6ba96cf11ac58cf39396bf53c6fbfe1be958
pakowanko <wojtek@pakowanko.com>
2025-07-14 23:21:48 +0200

    FIX: Włączenie obsługi SSL dla połączenia z bazą danych

--- db.js
+++ db.js
@@ -5,10 +5,6 @@ console.log('Łączenie z bazą danych przez DATABASE_URL...');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ZMIANA: Włączamy wymagane połączenie SSL
-  ssl: {
-    rejectUnauthorized: false
-  }
 });
 
 module.exports = pool;

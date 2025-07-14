2c92d48148c8c83f0958c091b241937607f0e692
pakowanko <wojtek@pakowanko.com>
2025-07-14 23:24:27 +0200

    FIX: Włączenie obsługi SSL dla połączenia z bazą danych

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 // db.js
 const { Pool } = require('pg');
 
-console.log('Łączenie z bazą danych przez DATABASE_URL...');
+console.log('Łączenie z bazą danych przez parametry...');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
 });
 
 module.exports = pool;

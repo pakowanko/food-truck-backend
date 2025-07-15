8c3278a6c1eba8808117c41728106dde83b5bdd1
pakowanko <wojtek@pakowanko.com>
2025-07-15 18:07:54 +0200

    FIX: Poprawka AuthController

--- db.js
+++ db.js
@@ -1,13 +1,4 @@
 // db.js
 const { Pool } = require('pg');
-
-console.log('Łączenie z bazą danych przez DATABASE_URL z SSL...');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 module.exports = pool;

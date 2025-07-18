0b75376684deb4893b5b953f5fc248db4491eb2b
pakowanko <wojtek@pakowanko.com>
2025-07-18 15:47:52 +0200

    Dodano nowe funkcje i poprawki

--- routes/gusRoutes.js
+++ routes/gusRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const gusController = require('../controllers/gusController');
 const authenticateToken = require('../middleware/authenticateToken');
 
+// Dodajemy ten log, aby sprawdzić, czy plik jest w ogóle uruchamiany
+console.log('--- Plik gusRoutes.js został pomyślnie załadowany! ---');
+
 // Definiujemy ścieżkę, która przyjmuje NIP jako parametr
 router.get('/company-data/:nip', authenticateToken, gusController.getCompanyDataByNip);
 

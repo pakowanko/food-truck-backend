b0adab84cdaa9096fcf12bb46afafc4f99803730
pakowanko <wojtek@pakowanko.com>
2025-08-05 12:21:54 +0200

    Dodano funkcję publikacji na Facebooku

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -22,16 +22,14 @@ router.put('/users/:userId', adminController.updateUser);
 router.put('/bookings/:requestId/packaging-status', adminController.updatePackagingStatus);
 router.put('/bookings/:requestId/commission-status', adminController.updateCommissionStatus);
 
-// --- NOWA TRASA do aktualizacji szczegółów profilu (w tym promienia) ---
+// --- KLUCZOWA TRASA: Upewnij się, że ta linia istnieje i jest poprawna ---
 router.put('/profiles/:profileId/details', adminController.updateProfileDetails);
 
 // Trasy do usuwania danych
 router.delete('/users/:userId', adminController.deleteUser);
 router.delete('/profiles/:profileId', adminController.deleteProfile);
 
-// Trasa do webhooka Stripe - powinna być wyłączona z middleware autoryzacji, jeśli jest publiczna
-// W tym przypadku zakładamy, że jest w tym samym pliku i wymaga uprawnień admina do podglądu
-// UWAGA: W produkcji webhook Stripe powinien być publicznym endpointem bez authMiddleware!
+// Trasa do webhooka Stripe
 router.post('/stripe-webhook', express.raw({type: 'application/json'}), adminController.handleStripeWebhook);
 
 // Trasa do jednorazowej synchronizacji

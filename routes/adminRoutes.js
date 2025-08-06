f5e877a6f15c46adf95016689be4824fd3a48d96
pakowanko <wojtek@pakowanko.com>
2025-08-06 07:11:53 +0200

    Uodporniony formularz rejestracji

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -16,14 +16,19 @@ router.get('/conversations', adminController.getAllConversations);
 router.get('/conversations/:conversationId/messages', adminController.getConversationMessages);
 router.get('/users/:userId/profiles', adminController.getUserProfiles);
 
+// --- NOWA TRASA: Pobieranie pełnych danych profilu do edycji ---
+// Ta linia jest kluczowa, aby okno edycji mogło pobrać dane.
+router.get('/profiles/:profileId', adminController.getProfileForAdmin);
+
 // Trasy do aktualizacji i modyfikacji danych
 router.put('/users/:userId/toggle-block', adminController.toggleUserBlock);
 router.put('/users/:userId', adminController.updateUser);
 router.put('/bookings/:requestId/packaging-status', adminController.updatePackagingStatus);
 router.put('/bookings/:requestId/commission-status', adminController.updateCommissionStatus);
-
-// --- NOWA TRASA do aktualizacji szczegółów profilu (w tym promienia) ---
 router.put('/profiles/:profileId/details', adminController.updateProfileDetails);
+
+// --- NOWA TRASA: Usuwanie pojedynczego zdjęcia ---
+router.delete('/profiles/:profileId/photo', adminController.deleteProfilePhoto);
 
 // Trasy do usuwania danych
 router.delete('/users/:userId', adminController.deleteUser);
